5dca31da9d06388f35186b8cabd63c8ca35980a6
philopian <pdotwdot@gmail.com>
2014-12-12 20:42:27 -0800

    added 2 properties: myMarker & coordinates

--- Mapkit Custom Callouts/MapViewController.h
+++ Mapkit Custom Callouts/MapViewController.h
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import "PWAnnotation.h"
 
 @interface MapViewController : UIViewController <MKMapViewDelegate>
-
+@property (nonatomic,strong)PWAnnotation *myMarker;
+@property CLLocationCoordinate2D coordinates;
 @end
