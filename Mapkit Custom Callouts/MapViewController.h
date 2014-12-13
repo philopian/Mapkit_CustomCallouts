e860be6d2b7e3c5f25b30d3f42ffac10089040ce
philopian <pdotwdot@gmail.com>
2014-12-12 20:36:27 -0800

    project setup

---
+++ Mapkit Custom Callouts/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Mapkit Custom Callouts
+//
+//  Created by Philippe Willis on 12/12/14.
+//  Copyright (c) 2014 Philippe Willis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
